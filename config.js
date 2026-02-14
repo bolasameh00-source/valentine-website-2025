b1f3df8aea9cfa36457a683f60cfeed8fa023232
bolasameh00-source <bolasameh00@gmail.com>
2026-02-14 23:40:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026 YA LOLO ? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Happy Valentine’s Day, Berry ❤️ You make my days brighter and my heart happier just by being you I’m so lucky to have you. 🌹",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
